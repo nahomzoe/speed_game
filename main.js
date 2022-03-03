c233c391ed358dd8123194db652094bcf4e3e181
Asfaw Nahom <s2200108@edu.bc.fi>
2022-03-03 15:50:37 +0200

    first commit

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const endButton = document.querySelector("#stop");
 const overlay = document.querySelector("#overlay");
 const closeButton = document.querySelector("#close");
 const circles = document.querySelectorAll(".circle");
+const scoreText = document.querySelector("#score");
+const resultText = document.querySelector("#result");
+
+let active = 0;
+let score = 0;
+let pace = 1000;
+let rounds = 0;
+let timer;
 
 const getRndInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,17 +21,60 @@ circles.forEach((circle, i) => {
 });
 
 const clickedCircle = (i) => {
-  i += 1;
   console.log("clicked circle was:", i);
+  if (i !== active) {
+    endGame();
+  } else {
+    score++;
+    rounds--;
+    scoreText.textContent = score;
+  }
 };
 
 const startGame = () => {
+  startButton.style.display = "none";
+  endButton.style.display = "inline";
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].style.pointerEvents = "auto";
+  }
   console.log("game started");
+
+  let nextActive = pickNew(active);
+  circles[nextActive].classList.toggle("active");
+  circles[active].classList.remove("active");
+
+  active = nextActive;
+  console.log("active circle:", active);
+  timer = setTimeout(startGame, pace);
+  pace = pace - 10;
+
+  if (rounds >= 1) {
+    endGame();
+  }
+  rounds++;
+
+  function pickNew(active) {
+    let nextActive = getRndInt(0, 3);
+
+    if (nextActive != active) {
+      return nextActive;
+    } else {
+      return pickNew(active);
+    }
+  }
 };
 
 const endGame = () => {
   console.log("game ended");
+  clearTimeout(timer);
   overlay.style.visibility = "visible";
+  if (score == 0) {
+    resultText.textContent = `Your score is ${score}. Try again, It is not the end of the world!`;
+  } else if (score > 1 || score < 10) {
+    resultText.textContent = `Your score is ${score}. Not bad, You can do even better`;
+  } else if (score > 11) {
+    resultText.textContent = `Your score is ${score}. You are amazing!`;
+  }
 };
 
 const reloadGame = () => {
